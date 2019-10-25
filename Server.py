8272351a626a60ed92c553b65d61a5a0ad0b2a60
Pandagud <pandagud@gmail.com>
2019-10-25 13:12:05 +0200

    Server updated

--- Server.py
+++ Server.py
@@ -2,32 +2,46 @@ from socket import *
 import sys
 import csv
 
+
+timesdatasend = 0;
+
 server = socket(AF_INET,SOCK_STREAM)
 ipadresse= gethostbyname(gethostname());
 print (ipadresse);
-server.bind(('',9998))
+server.bind(('',9997))
 print (server);
 server.listen(3);
-
+conn, addr = server.accept()     # Establish connection with client.
+print ('Got connection from', addr)
 while True:
-    conn, addr = server.accept()     # Establish connection with client.
-    print ('Got connection from', addr)
-    data = conn.recv(2048)
-    print('Server received', repr(data))
-    myFile = open('csvdata.csv', 'w')
-    strdata=str(data);
-    strdata=strdata.replace('b','');
-    strdata=strdata.replace('\'','');
-    with myFile:
-       writer = csv.writer(myFile)
-       myBitLenght=8;
-       
-       while myBitLenght < len(strdata):
-           oneRow=strdata[0:myBitLenght];
-           strdata=strdata.replace(strdata[0:myBitLenght],'');
-           writer.writerow(oneRow);
-    
-    
-
-server.close();
+    try:
+        rdata = conn.recv(1024)
+        if not rdata: break
+        
+        if timesdatasend == 0:
+            data=rdata;
+        else:
+            data+=rdata;
+        
+        print('Server received', repr(rdata))
+        timesdatasend = timesdatasend+1;
+        print("Client send this many times ",timesdatasend);
+    except Exception:
+        print("Something broke")
+    finally:
+        server.close();
+        
+        
+myFile = open('csvdata.csv', 'w')
+print(len(data))
+strdata=str(data);
+strdata=strdata.replace('b','');
+strdata=strdata.replace('\'','');
+with myFile:
+    writer = csv.writer(myFile)
+    myBitLenght=12;
+    while myBitLenght < len(strdata):
+        oneRow=strdata[0:myBitLenght];
+        strdata=strdata.replace(strdata[0:myBitLenght],'');
+        writer.writerow(oneRow);
 
