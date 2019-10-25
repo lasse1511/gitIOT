236758acc3130e84fc05ef6044b1a686c95bdea0
Lasse Bonner <lassebonner@hotmail.com>
2019-10-25 09:54:15 +0200

    sending whole file

--- client.py
+++ client.py
@@ -3,21 +3,25 @@ import sys
 
 s = socket.socket();
 s.connect(('192.168.0.102',9998))
-f = open ("../Downloads/GDProjectMaterial/unpacked/08730_01.csv", "rb")
+f = open ("../../Downloads/GDProjectMaterial/unpacked/08730_01.csv", "rb")
 l = f.read(1024)
-dataToSend = '';
-index = 0;
-print("This is l",l)
-l = str(l)
-for i in l:
-    try:
-        dataToSend = dataToSend +(str(int(i)));
-        index = index + 1;
-    except ValueError:
-        continue;
-l = str.encode(dataToSend)
-print("This is new l:",l);
+x = 1;
 while (l):
-    s.send(l)
+    dataToSend = '';
+    index = 0;
+    #print("This is l",l)
+    data = str(l)
+    for i in data:
+        try:
+            dataToSend = dataToSend +(str(int(i)));
+            index = index + 1;
+        except ValueError:
+            continue;
+    data = str.encode(dataToSend)
+    #print("This is new l:",data);
+    
+    print('Sending data ', x);
+    x = x+1;
+    s.send(data)
     l = f.read(1024)
 s.close()
