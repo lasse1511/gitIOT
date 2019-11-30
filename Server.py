7827fc6a6c28e52e168b7b546d3e76fa4379b9fa
Lasse Bonner <lassebonner@hotmail.com>
2019-11-30 13:12:48 +0100

    server coommit

--- Server.py
+++ Server.py
@@ -12,48 +12,33 @@ print (ipadresse);
 server.bind(('',9997))
 print (server);
 server.listen(10);
-conn, addr = server.accept()     # Establish connection with client.
+conn, addr = server.accept() # Establish connection with client.
 print ('Got connection from', addr)
 data = str("").encode()
 while True:
-    try:
-        rdata = conn.recv(1024)
-        if not rdata: break
-        data+=rdata;
+try:
+rdata = conn.recv(1024)
+if not rdata: break
+data+=rdata;
 
-        print('Server received', repr(rdata))
-        timesdatasend = timesdatasend+1;
-        print("Client send this many times ",timesdatasend);
-    except Exception:
-        print("Something broke")
-    finally:
-        server.close();
+print('Server received', repr(rdata))
+timesdatasend = timesdatasend+1;
+print("Client send this many times ",timesdatasend);
+except Exception:
+print("Something broke")
+finally:
+server.close();
 #if type(rdata)==str and title=false:
 #data=str(data)
 data = data.split(b'FILENAME:')
-print("Dette er vores array af filnavne:" , data)
-print("Deete har jeg modtager", data)
-print("Længden af data", len(data))
-print("Længden af data str", len(str(data).encode()))
-data=data.replace("\'b\'","")
-print(len(data.split("b")))
-data=data.split("FILENAME")
-print("Længden af data-array", len(data))
-for x in data:
-    if (x[0]==':'):
-        index = x.split("\\")
-        filename=index[0]
-        filename=filename[1:]
-        
-        data_to_save = str(x[len(filename)+1:]).encode()
-        #print(filename, data_to_save)
-        myFile = open(filename, 'wb')
-        myFile.write(data_to_save)
-        myFile.close()
-        print(data_to_save)
-        print(len(x))
-        print(filename," fylder ", len(data_to_save)," bits nu")
-    
-
-
-
+# print("Dette er vores array af filnavne:" , data)
+# print("Deete har jeg modtager", data)
+# print("Længden af data", len(data))
+# print("Længden af data str", len(str(data).encode()))
+for i in range(1,len(data)):
+filename, data_to_save = data[i].split(b'DATA:')
+filename = filename.decode('utf-8')
+myFile = open("compressed/"+filename, 'wb')
+myFile.write(data_to_save)
+myFile.close()
+print(filename," fylder ", len(data_to_save)," bytes nu")
